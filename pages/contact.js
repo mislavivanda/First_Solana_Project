6ab5bdca39c3e348f818ad98ddf543180dd509c5
mislavivanda <mislavivanda454@gmail.com>
2024-10-03 13:06:42 +0200

    Contantc page layout

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,147 @@
+import { Button, InputField } from "../components";
 const Contact = () => {
   return (
-    <>
+    <div>
       <h1 className="inline text-5xl font-extrabold  mx-5 sm:mx-10 w-full max-w-screen-xl text-left border-b-primary-color border-b-[5px] border-solid">
         Contact
       </h1>
-    </>
+      <section className="mt-10 flex flex-wrap justify-center sm:justify-around max-w-screen-lg mx-auto">
+        <div className="flex justify-evenly flex-col mb-10 sm:mb-0">
+          <div className="flex flex-col p-2">
+            <svg
+              x="0px"
+              y="0px"
+              viewBox="0 0 414.937 414.937"
+              style={{ width: "2.5rem", height: "2.5rem" }}
+            >
+              <g>
+                <path
+                  fill="#642dfd"
+                  d="M159.138,256.452c37.217,36.944,80.295,72.236,97.207,55.195c24.215-24.392,39.12-45.614,92.854-2.761
+                  c53.734,42.874,12.696,71.727-10.757,95.363c-27.064,27.269-128.432,1.911-228.909-97.804C9.062,206.71-17.07,105.54,10.014,78.258
+                  c23.46-23.637,52.006-64.879,95.254-11.458c43.269,53.394,22.161,68.462-2.054,92.861
+                  C86.31,176.695,121.915,219.501,159.138,256.452z M213.104,80.203c0,0-11.227-1.754-19.088,6.113
+                  c-8.092,8.092-8.445,22.032,0.082,30.552c5.039,5.039,12.145,6.113,12.145,6.113c13.852,2.598,34.728,6.997,56.944,29.206
+                  c22.209,22.208,26.608,43.084,29.206,56.943c0,0,1.074,7.106,6.113,12.145c8.521,8.521,22.46,8.174,30.552,0.082
+                  c7.861-7.86,6.113-19.087,6.113-19.087c-4.399-28.057-17.999-57.365-41.351-80.716C270.462,98.203,241.153,84.609,213.104,80.203z
+                  M318.415,96.958c40.719,40.719,58.079,86.932,52.428,124.379c0,0-1.972,11.859,5.773,19.604
+                  c8.718,8.718,22.535,8.215,30.695,0.062c5.243-5.243,6.385-13.777,6.385-13.777c4.672-32.361-1.203-97.464-64.647-160.901
+                  C285.605,2.887,220.509-2.988,188.147,1.677c0,0-8.527,1.136-13.777,6.385c-8.16,8.16-8.656,21.978,0.061,30.695
+                  c7.746,7.746,19.604,5.773,19.604,5.773C231.484,38.879,277.696,56.24,318.415,96.958z"
+                />
+              </g>
+            </svg>
+            <h4 className="text-lg text-font-color-dark font-extrabold mt-2">
+              (555) 123-4567
+            </h4>
+          </div>
+          <div className="flex flex-col p-2">
+            <svg
+              x="0px"
+              y="0px"
+              viewBox="0 0 400 400"
+              style={{ width: "2.5rem", height: "2.5rem" }}
+            >
+              <g>
+                <polygon
+                  fill="#642dfd"
+                  points="0,127.5 0,274.219 104.8,206.1 	"
+                />
+                <polygon
+                  fill="#642dfd"
+                  points="400,274.219 400,127.5 295.2,206.1 	"
+                />
+                <polygon
+                  fill="#642dfd"
+                  points="200,277.5 130.357,225.268 0,310 0,340 400,340 400,310 269.643,225.268 	"
+                />
+                <polygon
+                  fill="#642dfd"
+                  points="0,90 200,240 400,90 400,60 0,60"
+                />
+              </g>
+            </svg>
+
+            <h4 className="text-xl text-font-color-dark font-extrabold mt-2">
+              contact@boldmint.com
+            </h4>
+          </div>
+          <div className="flex flex-col p-2">
+            <svg
+              x="0px"
+              y="0px"
+              viewBox="0 0 264.018 264.018"
+              style={{ width: "2.5rem", height: "2.5rem" }}
+            >
+              <g>
+                <path
+                  fill="#642dfd"
+                  d="M132.009,0c-42.66,0-77.366,34.706-77.366,77.366c0,11.634,2.52,22.815,7.488,33.24c0.1,0.223,0.205,0.442,0.317,0.661
+                l58.454,113.179c2.146,4.154,6.431,6.764,11.106,6.764c4.676,0,8.961-2.609,11.106-6.764l58.438-113.148
+                c0.101-0.195,0.195-0.392,0.285-0.591c5.001-10.455,7.536-21.67,7.536-33.341C209.375,34.706,174.669,0,132.009,0z
+                M132.009,117.861c-22.329,0-40.495-18.166-40.495-40.495c0-22.328,18.166-40.494,40.495-40.494s40.495,18.166,40.495,40.494
+                C172.504,99.695,154.338,117.861,132.009,117.861z"
+                />
+                <path
+                  fill="#642dfd"
+                  d="M161.81,249.018h-59.602c-4.143,0-7.5,3.357-7.5,7.5c0,4.143,3.357,7.5,7.5,7.5h59.602c4.143,0,7.5-3.357,7.5-7.5
+                C169.31,252.375,165.952,249.018,161.81,249.018z"
+                />
+              </g>
+            </svg>
+            <h4 className="text-xl text-font-color-dark font-extrabold mt-2">
+              221B Baker Street
+              <br />
+              Mysterville
+              <br />
+              Detective District 00001
+            </h4>
+          </div>
+          <div className="flex flex-col p-2">
+            <svg
+              x="0px"
+              y="0px"
+              viewBox="0 0 466.008 466.008"
+              style={{ width: "2.5rem", height: "2.5rem" }}
+            >
+              <g>
+                <path
+                  fill="#642dfd"
+                  d="M233.004,0C104.224,0,0,104.212,0,233.004c0,128.781,104.212,233.004,233.004,233.004
+                  c128.782,0,233.004-104.212,233.004-233.004C466.008,104.222,361.796,0,233.004,0z M244.484,242.659l-63.512,75.511
+                  c-5.333,6.34-14.797,7.156-21.135,1.824c-6.34-5.333-7.157-14.795-1.824-21.135l59.991-71.325V58.028c0-8.284,6.716-15,15-15
+                  s15,6.716,15,15v174.976h0C248.004,236.536,246.757,239.956,244.484,242.659z"
+                />
+              </g>
+            </svg>
+            <h4 className="text-xl text-font-color-dark font-extrabold mt-2">
+              Mon - Sat: 8am - 8pm <br />
+              Sun: Closed
+            </h4>
+          </div>
+        </div>
+        <div className="flex flex-col text-font-color">
+          <h2 className="text-xl text-font-color-dark font-extrabold">
+            Contact us directly:
+          </h2>
+          <InputField type="text" placeholder="Your Name" classes="mt-4 p-2" />
+          <InputField
+            type="text"
+            placeholder="Your E-mail"
+            classes="mt-4 p-2"
+          />
+          <textarea
+            className="w-full mt-4 p-2 focus:border-primary-color border-[1px] border-solid transition-colors rounded-md"
+            placeholder="Your Message"
+            cols="30"
+            rows="5"
+          ></textarea>
+          <Button type="filled" classes="mt-4">
+            Submit
+          </Button>
+        </div>
+      </section>
+    </div>
   );
 };
 
